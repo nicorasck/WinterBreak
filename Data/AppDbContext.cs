898fc59fd0c145b49719380e0f564d54e7611d62
nicorasck <nicholas.rasck@gmail.com>
2024-12-20 00:24:48 +0100

    update commit

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -4,11 +4,11 @@ using Models;
 
 public class AppDbContext : DbContext
 {
-    public DbSet<Employee> Employees {get; set;}            // A list to stor Employees.
+    public DbSet<Employee> Employees {get; set;}            // A list to store Employees.
     public DbSet<Patient> Patients {get; set;}              // A list to store Patients.
     public DbSet<Address> Addresses {get; set;}             // A list to store Addresses.
     public DbSet<Room> Rooms {get; set;}                    // A list to store Rooms.
-    public DbSet<RoomPatient> RoomPatients {get; set;}      // A list to sore RoomPatients.
+    public DbSet<RoomPatient> RoomPatients {get; set;}      // A list to store RoomPatients.
     public DbSet<TestReport> TestReports {get; set;}        // A list to store TestReports.
     public DbSet<Invoice> Invoices {get; set;}              // A list to store Invoices.
     public DbSet<Record> Records {get; set;}                // A list to Store Records.
