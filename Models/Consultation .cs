bfe689466189076df27dcdfd179c67c3fa2c4573
nicorasck <nicholas.rasck@gmail.com>
2024-12-21 13:07:23 +0100

    update commit

--- Models/Consultation .cs
+++ Models/Consultation .cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Models
 {
-    public class RoomPatient
+    public class Consultation
     {
         public int RoomID {get; set;}                       // Foreign Key => references to the entity o Room.
         public int PatientID {get; set;}                    // Foreign Key => references to the entity of Patent.
--- Models/Room.cs
+++ Models/Room.cs
@@ -13,6 +13,6 @@ namespace Models
         public int RoomID {get; set;}                  // Primary Key => each room must be unique
         public required string RoomType {get; set;}    // each room must describe what sort of room it is (Enum)
         public required int RoomCapacity {get; set;}   // How many patients it can be in the room at the same tome
-        public bool RoomIsAvailable {get; set;}        // default should be True
+        public bool IsAvailable {get; set;}        // default should be True
     }
 }
